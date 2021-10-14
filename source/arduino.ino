bb3219944a8ca9888ef49616d7acadd0fc1cfe97
HyeokjuLee <hyeokju1313@gmail.com>
2021-10-14 13:12:01 +0900

    Revert "Update arduino.ino"

--- source/arduino.ino
+++ source/arduino.ino
@@ -1,97 +1,61 @@
-//Sample using LiquidCrystal library
 #include <LiquidCrystal.h>
+#define GOTOWORK 7
+#define OFFWORK 6
+#define ENROLLMENT 5
+#define REMOVE 4
 
-/*******************************************************
+LiquidCrystal lcd{13, 12, 11, 10, 9, 8};
 
-This program will test the LCD panel and the buttons
-Mark Bramwell, July 2010
-
-********************************************************/
-
-// select the pins used on the LCD panel
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
-
-// define some values used by the panel and buttons
-int lcd_key     = 0;
-int adc_key_in  = 0;
-#define btnRIGHT  0
-#define btnUP     1
-#define btnDOWN   2
-#define btnLEFT   3
-#define btnSELECT 4
-#define btnNONE   5
-
-// read the buttons
-int read_LCD_buttons()
-{
- adc_key_in = analogRead(0);      // read the value from the sensor
- // my buttons when read are centered at these valies: 0, 144, 329, 504, 741
- // we add approx 50 to those values and check to see if we are close
- //if (adc_key_in > 1000) return btnNONE; // We make this the 1st option for speed reasons since it will be the most likely result
- // For V1.1 us this threshold
- if (adc_key_in < 50)   return btnRIGHT;
- if (adc_key_in < 250)  return btnUP;
- if (adc_key_in < 450)  return btnDOWN;
- if (adc_key_in < 650)  return btnLEFT;
- if (adc_key_in < 850)  return btnSELECT;
-
- // For V1.0 comment the other threshold and use the one below:
-/*
- if (adc_key_in < 50)   return btnRIGHT;
- if (adc_key_in < 195)  return btnUP;
- if (adc_key_in < 380)  return btnDOWN;
- if (adc_key_in < 555)  return btnLEFT;
- if (adc_key_in < 790)  return btnSELECT;
-*/
-
-
- return btnNONE;  // when all others fail, return this...
+void setup() {
+  lcd.begin(16, 2);
+  //Serial.begin(9600);
+  pinMode(GOTOWORK, INPUT);
+  pinMode(OFFWORK, INPUT);
+  pinMode(ENROLLMENT, INPUT);
+  pinMode(REMOVE, INPUT);
 }
 
-void setup()
-{
- lcd.begin(16, 2);              // start the library
- lcd.setCursor(0,0);
- lcd.print("clock"); // print a simple message
+void loop() {
+  //int value = digitalRead(GOTOWORK);
+  
+  //int value = digitalRead(GOTOWORK);
+  //Serial.println(digitalRead(GOTOWORK));
+  
+  if (digitalRead(GOTOWORK) == LOW) {
+    printGoToWork();
+  }
+  else if (digitalRead(OFFWORK) == LOW) {
+    printOffWork();
+  }
+  else if (digitalRead(ENROLLMENT) == LOW) {
+    printEnrollment();
+  }
+  else if (digitalRead(REMOVE) == LOW) {
+    printRemove();
+  }
+  
 }
 
-void loop()
-{
- lcd.setCursor(0,1);            // move to the begining of the second line
- lcd_key = read_LCD_buttons();  // read the buttons
+void printGoToWork() {
+  lcd.setCursor(0, 0);
+  lcd.clear();
+  lcd.print("go to work");
+}
 
- switch (lcd_key)               // depending on which button was pushed, we perform an action
- {
-   case btnRIGHT:
-     {
-     lcd.print("Remove     ");
-     break;
-     }
-   case btnLEFT:
-     {
-     lcd.print("Enrollment");
-     break;
-     }
-   case btnUP:
-     {
-     lcd.print("Go to work");
-     break;
-     }
-   case btnDOWN:
-     {
-     lcd.print("Off work  ");
-     break;
-     }
-   case btnSELECT:
-     {
-     lcd.print("SELECT    ");
-     break;
-     }
-     /*case btnNONE:
-     {
-     lcd.print("NONE      ");
-     break;
-     }*/
- }
+void printOffWork() {
+  lcd.setCursor(0, 0);
+  lcd.clear();
+  lcd.print("off work");
+}
 
+void printEnrollment() {
+  lcd.setCursor(0, 0);
+  lcd.clear();
+  lcd.print("enrollment");
+}
+
+void printRemove() {
+  lcd.setCursor(0, 0);
+  lcd.clear();
+  lcd.print("remove");
 }
