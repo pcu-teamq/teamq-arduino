2a3380a1232e0d3759a6221ca9c1d8bd63df8d7f
mofxxkerhun <90238606+paichaiboy@users.noreply.github.com>
2021-10-14 13:35:31 +0900

    create button

--- source/arduino.ino
+++ source/arduino.ino
@@ -2,13 +2,31 @@
 #include <Wire.h>
 #include <LiquidCrystal.h>
 
+#define btnRIGHT  0
+#define btnUP     1
+#define btnDOWN   2
+#define btnLEFT   3
+#define btnSELECT 4
+#define btnNONE   5
+
 virtuabotixRTC myRTC(6, 7, 8);
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 int lcd_key     = 0;
 int adc_key_in  = 0;
 
+int read_LCD_buttons()
+{
+ adc_key_in = analogRead(0)
+ if (adc_key_in < 50)   return btnRIGHT;
+ if (adc_key_in < 250)  return btnUP;
+ if (adc_key_in < 450)  return btnDOWN;
+ if (adc_key_in < 650)  return btnLEFT;
+ if (adc_key_in < 850)  return btnSELECT;
+ return btnNONE;
+}
+
 void setup()  {
-  lcd.begin(16, 2);              // start the library
+  lcd.begin(16, 2);
   lcd.setCursor(0,0);
   myRTC.setDS1302Time(00, 20, 42, 1, 13, 10, 2021);
 }
@@ -50,4 +68,31 @@ void loop()  {
   lcd.print(myRTC.minutes);
 
   delay(1000);
+
+  lcd.setCursor(0,1);
+ lcd_key = read_LCD_buttons();
+
+ switch (lcd_key)
+ {
+   case btnRIGHT:
+     {
+     lcd.print(" Remove     ");
+     break;
+     }
+   case btnLEFT:
+     {
+     lcd.print(" Enrollment");
+     break;
+     }
+   case btnUP:
+     {
+     lcd.print(" Go to work");
+     break;
+     }
+   case btnDOWN:
+     {
+     lcd.print(" Off work  ");
+     break;
+     }
+ }
 }
